6dd989d73d95d2e7104beb11b1b8c44fb93ed343
lgc-s <luisguilhermecs3@gmail.com>
2025-04-04 17:00:09 -0300

    update header

--- script/script.js
+++ script/script.js
@@ -1,54 +1,56 @@
 document.getElementById('box').addEventListener('click', () => {
     const boxButton = document.querySelector('.box-open');
     let contentDiv = document.querySelector('.open-box');
-    
+
     if (!contentDiv) {
         contentDiv = document.createElement('div');
         contentDiv.classList.add('open-box');
         contentDiv.innerHTML = `
-        <div class="menu-tab-temp-mobile">
-            <button class="button-temp">Início</button>
-            <button class="button-temp">Botão 2</button>
-            <button class="button-temp">Botão 3</button>
-            <button class="button-temp">Botão 4</button>
-            <button class="button-temp">Botão 5</button>
-            <button class="button-temp">Botão 6</button>
+        <div class="menu-tab-mobile">
+            <button class="button-mobile"><b>Início</b></button>
+            <button class="button-mobile"><b>Sobre</b></button>
+            <button class="button-mobile"><b>Ministérios</b></button>
+            <button class="button-mobile"><b>Ação Social</b></button>
+            <button class="button-mobile"><b>NED</b></button>
+            <button class="button-mobile"><b>Programação</b></button>
         </div>`;
 
         contentDiv.style.opacity = "0";
         contentDiv.style.transition = "opacity 0.5s ease";
-        
+
         boxButton.appendChild(contentDiv);
-        
-            const links = contentDiv.querySelectorAll("a");
-            links.forEach((link) => {
-                link.style.pointerEvents = "none";
-            });
-            
-            requestAnimationFrame(() => {
-                contentDiv.style.opacity = "1";
-                
-                setTimeout(() => {
-                    links.forEach((link) => {
-                        link.style.pointerEvents = "auto";
-                    });
-                }, 500);
-            });
-            
-            const handleOutsideClick = (event) => {
-                if (!contentDiv.contains(event.target) && event.target.id !== 'box') {
-                        links.forEach((link) => {
-                            link.style.pointerEvents = "none";
-                        });
-                        
-                        contentDiv.style.opacity = "0";
-                        
-                        setTimeout(() => {
-                            contentDiv.remove();
-                            document.removeEventListener('click', handleOutsideClick);
-                        }, 500);
-                    }
-                };
-                document.addEventListener('click', handleOutsideClick);
-            }
+
+        const buttons = contentDiv.querySelectorAll("box");
+        buttons.forEach((button) => {
+            button.style.pointerEvents = "none";
         });
+
+        requestAnimationFrame(() => {
+            contentDiv.style.opacity = "1";
+
+            setTimeout(() => {
+                buttons.forEach((button) => {
+                    button.style.pointerEvents = "auto";
+                });
+            }, 500);
+
+            const handleOutsideClick = (event) => {
+                if (contentDiv && !contentDiv.contains(event.target) && event.target.id !== 'box') {
+                    buttons.forEach((button) => {
+                        button.style.pointerEvents = "none";
+                    });
+
+                    contentDiv.style.opacity = "0";
+
+                    setTimeout(() => {
+                        if (contentDiv && contentDiv.parentNode) {
+                            contentDiv.remove();
+                        }
+                        document.removeEventListener('click', handleOutsideClick);
+                    }, 500);
+                }
+            };
+            document.addEventListener('click', handleOutsideClick);
+        });
+    }
+});
